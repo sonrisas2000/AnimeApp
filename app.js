0bff12f2cc55f1cf12a948c7cb34601f18163c45
PauSaez <pau.saez4600@gmail.com>
2021-07-04 13:19:52 +0200

    Upload files

---
+++ app.js
@@ -0,0 +1,32 @@
+const buttonLike = document.getElementById('Good');
+let card = document.getElementById('card');
+const buttonDontLike = document.getElementById('Bad');
+
+
+function animarDontLike(){
+    if (card.className === 'card'){
+        card.className = 'card moveLeft-card';
+      } else {
+        card.className = 'card';
+      }
+}
+
+function animarLike(){
+    if (card.className === 'card'){
+        card.className = 'card moveRight-card'
+      } else {
+        card.className = 'card';
+      }
+}
+
+
+buttonDontLike.onclick = () => animarDontLike()
+
+
+buttonLike.onclick = () => animarLike()
+    
+
+
+
+
+
---
+++ fetch.js
@@ -0,0 +1,33 @@
+const base_url = 'https://api.jikan.moe/v3/anime/';
+let imgAnime = document.getElementById('imgAnime');
+let bgImg = document.getElementById('bg_img');
+let title = document.getElementById('title');
+let number = Math.floor(Math.random()*15+100);
+
+
+function pageLoaded (){
+
+    // Take Anime
+    fetch (`${base_url}${number}`)
+    .then(res=>res.json())
+    .then(data=>data.image_url)
+    .then(res=> imgAnime = imgAnime.setAttribute("src", res))
+    .catch(err=>console.warn(err.message));
+
+    // Take image for background
+    fetch(`${base_url}${number}`)
+    .then(res=>res.json())
+    .then(data=>data.image_url)
+    .then(res=> bgImg = bgImg.setAttribute("src", res))
+    .catch(err=>console.warn(err.message));
+
+    // Take name anime
+    fetch(`${base_url}${number}`)
+    .then(res=>res.json())
+    .then(data=>data.title)
+    .then(res=> title = title.innerText = (res))
+    .catch(err=>console.warn(err.message));
+}
+window.addEventListener("load", pageLoaded);
+
+
